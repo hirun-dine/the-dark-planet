7cde428861992a4a0a469f809a6c7e01ca35476d
hirun-dine <xx.spectrumdragon.xx@gmail.com>
2026-08-26 16:19:32 -0400

    Update masterlist.js

--- styles/js/pages/masterlist.js
+++ styles/js/pages/masterlist.js
@@ -23,20 +23,19 @@ document.addEventListener("DOMContentLoaded", async () => {
             listData.profileArray[0].masterlistlog,
             charadex.page.masterlist.relatedData['masterlist log']
           );
+		  let backgroundElement = $('.cd-import-background');
+			if (listData.type == 'profile') {
+			  backgroundElement.attr('style', `background-image: url(${listData.profileArray[0].image})`);
+			} else {
+			  backgroundElement.each(function(i) {
+				const image = listData.array[i]?.image;
+				$(this).attr('style', `background-image: url(${bgurl})`);
+			  });
+			}
         }
-	  
-		let backgroundElement = $('.cd-import-background');
-		if (listData.type == 'profile') {
-		  backgroundElement.attr('style', `background-image: url(${listData.profileArray[0].image})`);
-		} else {
-		  backgroundElement.each(function(i) {
-			const image = listData.array[i]?.image;
-			$(this).attr('style', `background-image: url(${bgurl})`);
-		  });
-		}
 		
       }
-	  
+
     }
   );
   

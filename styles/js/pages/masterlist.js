d7e81dd5c1e61fcf6559a08f371724be314a98c5
hirun-dine <xx.spectrumdragon.xx@gmail.com>
2026-08-26 16:13:23 -0400

    Update masterlist.js

--- styles/js/pages/masterlist.js
+++ styles/js/pages/masterlist.js
@@ -42,48 +42,4 @@ document.addEventListener("DOMContentLoaded", async () => {
   
   charadex.tools.loadPage('.softload', 500);
   
-});/* ==================================================================== */
-/* Import Charadex
-======================================================================= */
-import { charadex } from '../charadex.js';
-
-
-/* ==================================================================== */
-/* Load
-======================================================================= */
-document.addEventListener("DOMContentLoaded", async () => {
-
-  let dex = await charadex.initialize.page(
-    null,
-    charadex.page.masterlist,
-    null, 
-    async (listData) => {
-
-      if (listData.type == 'profile') {
-
-        // Create the log dex
-        if (charadex.tools.checkArray(listData.profileArray[0].masterlistlog)) {
-          let logs = await charadex.initialize.page(
-            listData.profileArray[0].masterlistlog,
-            charadex.page.masterlist.relatedData['masterlist log']
-          );
-        }
-	  
-		let backgroundElement = $('.cd-import-background');
-		if (listData.type == 'profile') {
-		  backgroundElement.attr('style', `background-image: url(${listData.profileArray[0].image})`);
-		} else {
-		  backgroundElement.each(function(i) {
-			const image = listData.array[i]?.image;
-			$(this).attr('style', `background-image: url(${image})`);
-		  });
-		}
-		
-      }
-	  
-    }
-  );
-  
-  charadex.tools.loadPage('.softload', 500);
-  
 });

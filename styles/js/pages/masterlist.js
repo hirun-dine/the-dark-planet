c970a41a1aa4767e21ca54175fbfca233ca47e86
hirun-dine <xx.spectrumdragon.xx@gmail.com>
2026-08-26 16:06:34 -0400

    Update masterlist.js

--- styles/js/pages/masterlist.js
+++ styles/js/pages/masterlist.js
@@ -27,10 +27,10 @@ document.addEventListener("DOMContentLoaded", async () => {
 	  
 		let backgroundElement = $('.cd-import-background');
 		if (listData.type == 'profile') {
-		  backgroundElement.attr('style', `background-image: url(${listData.profileArray[0].bgurl})`);
+		  backgroundElement.attr('style', `background-image: url(${listData.profileArray[0].image})`);
 		} else {
 		  backgroundElement.each(function(i) {
-			const bgurl = listData.array[i]?.bgurl;
+			const image = listData.array[i]?.bgurl;
 			$(this).attr('style', `background-image: url(${bgurl})`);
 		  });
 		}

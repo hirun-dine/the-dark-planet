a642c00833b0662b4813bb0f1ea65477b3582e9c
hirun-dine <xx.spectrumdragon.xx@gmail.com>
2026-08-26 16:08:17 -0400

    Update masterlist.js

--- styles/js/pages/masterlist.js
+++ styles/js/pages/masterlist.js
@@ -30,8 +30,8 @@ document.addEventListener("DOMContentLoaded", async () => {
 		  backgroundElement.attr('style', `background-image: url(${listData.profileArray[0].image})`);
 		} else {
 		  backgroundElement.each(function(i) {
-			const image = listData.array[i]?.bgurl;
-			$(this).attr('style', `background-image: url(${bgurl})`);
+			const image = listData.array[i]?.image;
+			$(this).attr('style', `background-image: url(${image})`);
 		  });
 		}
 		

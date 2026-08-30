c447c3d0021be6131044124870be761822f7ce1b
hirun-dine <xx.spectrumdragon.xx@gmail.com>
2026-08-30 14:16:48 -0400

    Update config.js

--- styles/js/config.js
+++ styles/js/config.js
@@ -553,7 +553,7 @@ charadex.page.handlers = {
     [charadex.sheet.pages.inventoryLog]: {
 
       sheetPage: charadex.sheet.pages.inventoryLog,
-      sitePage: 'inventories',
+      sitePage: 'handlers',
       primaryProperty: 'handler',
       relatedProperty: 'handler',
       dexSelector: 'log',

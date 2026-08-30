7510b6a879a74e54fbd0f4e7d6e9b76d87769c46
hirun-dine <xx.spectrumdragon.xx@gmail.com>
2026-08-30 12:32:18 -0400

    Update handlers.js

--- styles/js/pages/handlers.js
+++ styles/js/pages/handlers.js
@@ -4,10 +4,39 @@
 import { charadex } from '../charadex.js';
 
 
-/* ==================================================================== */
-/* Load
-======================================================================= */
 document.addEventListener("DOMContentLoaded", async () => {
-  let dex = await charadex.initialize.page(null, charadex.page.handlers);
+
+  let dex = await charadex.initialize.page(
+    null,
+    charadex.page.handlers,
+    null, 
+    async (listData) => {
+
+      if (listData.type == 'profile') {
+
+        let profile = listData.profileArray[0];
+
+        // Designs
+        if (charadex.tools.checkArray(profile.masterlist)) {
+          let designs = await charadex.initialize.page(
+            profile.masterlist,
+            charadex.page.inventory.relatedData['handler'],
+          );
+        }
+
+        // Logs
+        if (charadex.tools.checkArray(profile.inventorylog)) {
+          let logs = await charadex.initialize.page(
+            profile.inventorylog,
+            charadex.page.inventory.relatedData['inventory log'],
+          );
+        }
+
+
+      }
+    }
+  );
+  
   charadex.tools.loadPage('.softload', 500);
+  
 });

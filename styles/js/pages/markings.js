41e9e10bc9bb994770395fbd524f003b99977e42
hirun-dine <xx.spectrumdragon.xx@gmail.com>
2026-09-06 09:51:50 -0400

    Update and rename faqs.js to markings.js

--- styles/js/pages/markings.js
+++ styles/js/pages/markings.js
@@ -10,10 +10,10 @@ import { charadex } from '../charadex.js';
 document.addEventListener("DOMContentLoaded", async () => {
   let dex = await charadex.initialize.page(
     null, 
-    charadex.page.faq, 
+    charadex.page.markings, 
     (arr) => {
 
-      let pageUrl = charadex.url.getPageUrl(charadex.page.faq.sitePage);
+      let pageUrl = charadex.url.getPageUrl(charadex.page.markings.sitePage);
       for (let question of arr) {
 
         // Make the tags pretty and actually work <3
